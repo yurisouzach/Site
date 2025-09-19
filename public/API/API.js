01813c81c0138b0d6c12a3855825bea972cd6c28
yurisouzach <chassotyuri@gmail.com>
2025-09-19 14:38:00 -0300

    multer?

--- public/API/API.js
+++ public/API/API.js
@@ -171,6 +171,7 @@ app.get('/ObterPresentes', async (req, res) => {
   }
 })
 
+const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
